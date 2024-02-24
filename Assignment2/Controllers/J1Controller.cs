efa0947b4d523fbe2022892a893098a49c49f5bf
lyeap <lyeapaul@gmail.com>
2024-02-24 02:15:01 -0600

    Add project files.

---
+++ Assignment2/Controllers/J1Controller.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace Assignment2.Controllers
+{
+    public class J1Controller : ApiController
+    {
+        /// <summary>
+        /// Calculates the total calories of the meal based on selected items.
+        /// </summary>
+        /// <param name="burger">
+        /// Integer representing the burger choice.
+        /// </param>
+        /// <param name="drink">
+        /// Integer representing the drink choice.
+        /// </param>
+        /// <param name="side">
+        /// Integer representing the side choice.
+        /// </param>
+        /// <param name="dessert">
+        /// Integer representing the dessert choice.
+        /// </param>
+        /// <returns>
+        /// Total calorie count of the meal.
+        /// </returns>
+        /// <example>
+        /// GET api/J1/Menu/4/4/4/4 => "Your total calorie count is 0"
+        /// </example>
+        /// <example>
+        /// GET api/J1/Menu/1/2/3/4 => "Your total calorie count is 691"
+        /// </example>
+        [HttpGet]
+        [Route("api/J1/Menu/{burger}/{drink}/{side}/{dessert}")]
+        public string TotalCalories(int burger, int drink, int side, int dessert)
+        {
+            int burgerCalorie = GetBurgerCalories(burger);
+            int drinkCalorie = GetDrinkCalories(drink);
+            int sideCalorie = GetSideCalories(side);
+            int dessertCalorie = GetDessertCalories(dessert);
+
+            int totalCalories = burgerCalorie + drinkCalorie + sideCalorie + dessertCalorie;
+
+            return $"Your total calorie count is {totalCalories}";
+        }
+        private int GetBurgerCalories(int choice)
+        {
+            switch (choice)
+            {
+                case 1: return 461;
+                case 2: return 431;
+                case 3: return 420;
+                default: return 0;
+            }
+        }
+        private int GetDrinkCalories(int choice)
+        {
+            switch (choice)
+            {
+                case 1: return 130;
+                case 2: return 160;
+                case 3: return 118;
+                default: return 0;
+            }
+        }
+        private int GetSideCalories(int choice)
+        {
+            switch (choice)
+            {
+                case 1: return 100;
+                case 2: return 57;
+                case 3: return 70;
+                default: return 0;
+            }
+        }
+        private int GetDessertCalories(int choice)
+        {
+            switch (choice)
+            {
+                case 1: return 167;
+                case 2: return 266;
+                case 3: return 75;
+                default: return 0;
+            }
+        }
+    }
+}
---
+++ Assignment2/Controllers/J2Controller.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace Assignment2.Controllers
+{
+    public class J2Controller : ApiController
+    {
+        /// <summary>
+        /// determines how many ways one can roll the value of 10 with two dice
+        /// </summary>
+        /// <param name="m">
+        /// total no of sides in first die
+        /// </param>
+        /// <param name="n">
+        /// total no of sides in second die
+        /// </param>
+        /// <returns>
+        /// number of ways to roll the value of 10
+        /// </returns>
+        /// <example>
+        /// GET api/J2/DiceGame/6/8 => "There are 5 total ways to get the sum 10."
+        /// </example>
+        /// <example>
+        /// GET api/J2/DiceGame/12/4 => "There are 4 ways to get the sum 10."
+        /// </example>
+        /// <example>
+        /// GET api/J2/DiceGame/3/3 => "There are 0 ways to get the sum 10."
+        /// </example>
+        /// <example>
+        /// GET api/J2/DiceGame/5/5 => "There is 1 way to get the sum 10."
+        /// </example>
+        [HttpGet]
+        [Route("api/J2/DiceGame/{m}/{n}")]
+        public string RollTen(int m, int n)
+        {
+            int count = 0;
+
+            // Iterate through all possible combinations
+            for (int i = 1; i <= m; i++)
+            {
+                for (int j = 1; j <= n; j++)
+                {
+                    // If the sum is 10,  count is incremented by 1
+                    if (i + j == 10)
+                    {
+                        count++;
+                    }
+                }
+            }
+
+            // Return message with the count
+           if (count == 1)
+            {
+                return "There is 1 way to get the sum 10.";
+            }
+            else
+            {
+                return $"There are {count} total ways to get the sum 10.";
+            }
+        }
+
+
+
+    }
+}
